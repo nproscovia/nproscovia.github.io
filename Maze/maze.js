cf8f39238f69e747eab04527e46b16cebd457a64
nproscovia <prossienakisozi@gmail.com>
2021-07-08 07:34:37 -0500

    adding

---
+++ Maze/maze.js
@@ -0,0 +1,29 @@
+$(function () {
+    "use strict";
+    $('#start').click(function () {
+        const $boundaries = $("#maze .boundary");
+        const $status = $('#status');
+        const $maze = $('#maze');
+        $boundaries.removeClass('youlose'); 
+        $status.text("Maze has begun! Find your way through");
+
+        $boundaries.mouseenter(function () {
+            $boundaries.addClass('youlose');
+        });
+
+        $maze.mouseleave(function () {
+           
+            $boundaries.addClass('youlose');
+        });
+
+        $('#end').mouseenter(function () {
+            if ($boundaries.hasClass('youlose')) {
+                $status.text("You lose :[");
+            } else {
+                $status.text("You win! :]");
+            }
+            $boundaries.off('mouseenter');
+            $maze.off('mouseleave');
+        });
+    });
+});
